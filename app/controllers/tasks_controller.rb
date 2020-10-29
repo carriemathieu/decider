0d4755dd455653a65d1cd9078bcab65d91013bd7
carriemathieu <carrieburks@gmail.com>
2020-10-29 14:05:40 -0400

    created new task form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TasksController < ApplicationController
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(task_params)
+        if @post.save
+            redirect_to @post
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:content)
+    end
 end
