84ceefe71bff71ed719ea727d67c458205457d36
carriemathieu <carrieburks@gmail.com>
2020-10-29 13:29:02 -0400

    navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
+    # gives views access to helper methods
+    helper_method :current_user, :logged_in?
+
+    
+    private 
 
     # if already defined, current_user = current_user. else, find current_user.
     def current_user
         @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
 
+    # if session assigned user_id
     def logged_in?
         !!session[:user_id]
     end
