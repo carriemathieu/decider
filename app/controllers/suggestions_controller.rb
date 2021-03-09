0422b612915504e7addbf11c4a7fd6c9b9e644b1
Carrie Mathieu <carrieburks@gmail.com>
2021-03-08 19:44:31 -0500

    refactored suggestions controller, added 2 helper methods: set_comment and redirect_if_not_suggestion_author

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,6 +1,8 @@
 class SuggestionsController < ApplicationController
     before_action :redirect_if_not_logged_in
-    
+    before_action :set_comment, only: [:show, :edit, :update]
+    before_action :redirect_if_not_suggestion_author, only: [:edit, :update]
+
     def index
         if params[:task_id] && @task = Task.find_by_id(params[:task_id]) # checks if post ID & id exists are true
             @suggestions = @task.suggestions
@@ -20,7 +22,6 @@ class SuggestionsController < ApplicationController
     end
 
     def create
-        #binding.pry
         @suggestion = current_user.suggestions.build(suggestion_params)
         if @suggestion.save
             redirect_to suggestions_path
@@ -31,10 +32,34 @@ class SuggestionsController < ApplicationController
 
     def show
     end
+
+    def edit 
+    end
+
+    def update
+        set_suggestion
+        if @suggestion.update(suggestion_params)
+            redirect_to suggestion_path(@suggestion)
+        else
+            render :edit
+        end
+    end
+
     private
     
     def suggestion_params
         params.require(:suggestion).permit(:content, :task_id)
+    end
+
+    def set_suggestion
+        @suggestion = Suggestion.find_by(id: params[:id])
+        if !@suggestion 
+            flash[:message] = "Task was not found."
+        end
+    end
+
+    def redirect_if_not_suggestion_author
+        redirect_to suggestions_path if @suggestion.user != current_user
     end
     # add all suggestions to an array
     # [array].sample OR [array].shuffle.first
