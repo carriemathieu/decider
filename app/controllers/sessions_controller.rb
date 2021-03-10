53ccf83fc7bf92116a16d337767544ca86965e98
Carrie Mathieu <carrieburks@gmail.com>
2021-03-09 22:27:20 -0500

    updated stying for tasks/new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
+            flash[:notice] = "Welcome, #{user.firstname}"
             redirect_to user_path(user)
         else
             flash[:alert] = "Incorrect login info, please try again"
