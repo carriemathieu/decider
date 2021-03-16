f66f98d945364cd0c0fc1d1b8636e155c2793361
Carrie Mathieu <carrieburks@gmail.com>
2021-03-16 12:44:00 -0400

    updated friendship index to show only users current_user not friends with

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ApplicationRecord
     belongs_to :user
     belongs_to :friend, :class_name => "User" # uses User model for association since friend model does not exist
+   
+
 end
