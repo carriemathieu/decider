9325528ebc42cb79c1d1298577159a804c80b48b
Carrie Mathieu <carrieburks@gmail.com>
2021-03-29 11:36:25 -0400

    updated readme

--- db/migrate/20210329144325_drop_category_table.rb
+++ db/migrate/20210329144325_drop_category_table.rb
@@ -1,4 +1,9 @@
 class DropCategoryTable < ActiveRecord::Migration[6.0]
-  def change
+  def up
+    drop_table :category
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
